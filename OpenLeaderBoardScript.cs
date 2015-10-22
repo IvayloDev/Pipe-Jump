0811a464ca17f668332eb0cdf2b36c93812a56a7
Ivaylo Palchev <ivopalchev@gmail.com>
2015-10-22 11:32:53 +0300

    Update OpenLeaderBoardScript.cs

--- OpenLeaderBoardScript.cs
+++ OpenLeaderBoardScript.cs
@@ -6,7 +6,7 @@ public class OpenLeaderBoardScript : MonoBehaviour {
 
 	public void OpenLeaderBoard () {
 	
-		Application.OpenURL("https://play.google.com/store/apps/details?id=com.IvayloDev.PipeJumper");
+		Application.OpenURL("");
 
 		}
 }
